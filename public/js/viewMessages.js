01955aebbec6228be17969731fe7844f45f6048a
mposey301 <finalhq376@gmail.com>
2021-07-22 16:54:06 +0000

    Add new lines

--- public/js/viewMessages.js
+++ public/js/viewMessages.js
@@ -1,4 +1,13 @@
 function getMessages() {
     const passcode = document.querySelector("#passcode");
+
+    const messagesRef = firebase.database().ref();
+    messagesRef.on('value', (snapshot) => {
+        const data = snapshot.val();
+        console.log(data);
+        for (let key in data) { 
+            console.log(key, data[key]);
+        }
+    });
     console.log("button pressed", passcode.value);
 }
