b29c1f51ec06520e0cf6e9945f24a40ed3f233d2
mposey301 <finalhq376@gmail.com>
2021-07-22 18:54:13 +0000

    Implement read and write to Firebase

--- public/js/viewMessages.js
+++ public/js/viewMessages.js
@@ -5,12 +5,11 @@ function getMessages() {
     messagesRef.on('value', (snapshot) => {
         const data = snapshot.val();
         console.log(data);
-        for (let key in data) { 
-            console.log(key, data[key]);
-            if (key === passcode.value) {
-                console.log("match found");
+        for (let key in data) {
+            if (data[key].passcode === passcode.value) {
+                console.log("Match found")
                 const message = document.querySelector("#message");
-                message.innerHTML = data[key]
+                message.innerHTML = data[key].message;
             }
         }
     });
---
+++ public/js/writeMessage.js
@@ -0,0 +1,10 @@
+const onSubmit = () => {
+    const passcodeInput = document.querySelector("#passcode").value;
+    const messageInput = document.querySelector("#message").value;
+    const payload = {
+        passcode: passcodeInput,
+        message: messageInput
+    };
+
+    firebase.database().ref().push(payload);
+}
